87a12a4a9a33fa0206c900ed7ec867c0153248ea
ar1b <arib.kamal@ryerson.ca>
2023-01-29 19:00:07 -0500

    new geo

--- 04-Local-Server/src/script.js
+++ 04-Local-Server/src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene()
-
+const newGeometry = new THREE.BoxGeometry(1,1,1)
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 0xFF0000})
 const mesh = new THREE.Mesh(geometry, material)
