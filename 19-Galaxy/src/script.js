d130be9a9f226e0e5e333a733c84c3045d6cccc2
Arib Kamal <arib.kamal@ryerson.ca>
2023-09-06 05:00:56 -0400

    added branches

--- 19-Galaxy/src/script.js
+++ 19-Galaxy/src/script.js
@@ -21,6 +21,8 @@ const parameters = {
 }
 parameters.count = 1000
 parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
 
 let geometry = null
 let material = null
@@ -38,11 +40,13 @@ const generateGalaxy = () => {
     //Creating the geometry
     for(let i=0; i<parameters.count; i++) {
         const i3 = i * 3
-        positions[i3 + 0] = ( Math.random() - 0.5) * 3
-        positions[i3 + 1] = ( Math.random() - 0.5) * 3
-        positions[i3 + 2] = ( Math.random() - 0.5) * 3
+        const radius = Math.random() * parameters.radius
+        const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2
+        positions[i3 + 0] = radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = 0
     }
-    
+
     geometry.setAttribute(
         'position',
         new THREE.BufferAttribute(positions, 3)
@@ -63,6 +67,8 @@ generateGalaxy()
 //adding tweakable parameters to gui menu
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
